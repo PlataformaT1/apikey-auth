90f49502cc792181e839530cb1607b81c5724fed
julio <julio.marrufo@t1comercios.com>
2025-04-15 16:47:47 -0600

    se agrega TLS a redis conexion 2

--- authorizer/app/pkg/redis/redis.go
+++ authorizer/app/pkg/redis/redis.go
@@ -65,7 +65,7 @@ func GetClient() *redis.Client {
 		redisClient = redis.NewClient(opts)
 
 		// Test the connection
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
 
 		_, err := redisClient.Ping(ctx).Result()
